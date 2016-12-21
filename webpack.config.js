6a23ed2772e06d1937829e36fe04444b4fa3a7f9
Darin Cassler <darin@cassler.net>
2016-12-20 22:57:14 -0500

    transform to webpack build tools, use npm for dependencies, set up webpack-dev-server

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+	entry: './app.js',
+
+	output: {
+		// path: 'build',
+		filename: 'bundle.js'
+	},
+
+	module: {
+		// transpile jsx
+		// transpile es2015 to js
+		loaders: [
+			{
+				test: /\.js$/ , 
+				exclude: /node_modules/,
+				loader: 'babel-loader'
+			}
+		]
+	}
+};
