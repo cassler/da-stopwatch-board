926b02481cf1948ff0f630169d85c0dd5a60158f
Darin Cassler <darin@cassler.net>
2016-12-21 12:10:05 -0500

    cleanup old files, add css build dir. small visual changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 // style
-import './app.scss';
+import './src/styles/app.scss';
 import Scoreboard from './src/containers/Scoreboard';
 
 render(
